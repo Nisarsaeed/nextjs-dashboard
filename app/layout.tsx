92b1f0bfb6d85872b7b41fc30995fb26e6f17805
Nisar Saeed <dash75549@gmail.com>
2024-07-07 21:06:53 +0500

    meta data added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts'
-
+import { Metadata } from 'next';
+ 
 export const experimental_ppr = true;
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+};
+
 
 export default function RootLayout({
   children,
